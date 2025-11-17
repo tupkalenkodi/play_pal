7fac60ab8e770a312f707af801601a508a2e9006
tupkalenkodi <tupkach@gmail.com>
2025-11-17 22:41:41 +0100

    Authentification: added validator, refined model, form and backend

--- accounts/backends.py
+++ accounts/backends.py
@@ -2,7 +2,9 @@ from django.contrib.auth.backends import BaseBackend
 from .models import CustomUser
 
 
+# CUSTOM BACKEND TO ALLOW AUTHENTICATION WITH E-MAIL, INSTEAD OF USERNAME
 class EmailBackend(BaseBackend):
+    # AUTHENTICATION WITH E-MAIL
     def authenticate(self, request, email=None, password=None, **kwargs):
         try:
             user = CustomUser.objects.get(email=email)
@@ -12,6 +14,7 @@ class EmailBackend(BaseBackend):
             return None
         return None
 
+    # NEEDED FOR DJANGO INTERNAL USE
     def get_user(self, user_id):
         try:
             return CustomUser.objects.get(pk=user_id)
